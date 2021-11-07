69d6248b42fa0b3989f5737c1307fb1e980e8c98
T4skar <79161107+T4skar@users.noreply.github.com>
2021-11-07 01:26:49 +0100

    Bumpers y La pelota esta en module player
    mucho texto

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -1,9 +1,13 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModulePlayer.h"
+#include "ModulePhysics.h"
+#include "ModuleRender.h"
+
 
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
+	
 }
 
 ModulePlayer::~ModulePlayer()
@@ -13,9 +17,17 @@ ModulePlayer::~ModulePlayer()
 bool ModulePlayer::Start()
 {
 	LOG("Loading player");
+
+	player();
+
 	return true;
 }
+void ModulePlayer::player() {
+	circles.add(App->physics->CreateCircle(550, 750, 15));
+	//App->renderer->Blit(circles, 550, 750);
 
+	circles.getLast()->data->listener = this;
+}
 // Unload assets
 bool ModulePlayer::CleanUp()
 {
